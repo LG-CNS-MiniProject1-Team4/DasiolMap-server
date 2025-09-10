f13145573dbe6ed63704024a21724c833a42e3d3
Startingfindmistakesamsung <dudgh2328@gmail.com>
2025-09-10 17:38:27 +0900

    create

---
+++ dasiolmap/src/main/java/com/dasiolmapserver/dasiolmap/DasiolmapApplication.java
@@ -0,0 +1,13 @@
+package com.dasiolmapserver.dasiolmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasiolmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasiolmapApplication.class, args);
+	}
+
+}
---
+++ dasiolmap/src/test/java/com/dasiolmapserver/dasiolmap/DasiolmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasiolmapserver.dasiolmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasiolmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
