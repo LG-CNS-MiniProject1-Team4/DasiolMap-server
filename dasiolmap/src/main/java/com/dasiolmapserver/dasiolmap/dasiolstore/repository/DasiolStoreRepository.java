86eca5817362d215a448e7a27495a6559ece5d59
InoPiaN <jh_95@naver.com>
2025-09-11 14:21:41 +0900

    FEAT-Table_Store

--- dasiolmap/src/main/java/com/dasiolmapserver/dasiolmap/dasiolstore/repository/DasiolStoreRepository.java
+++ dasiolmap/src/main/java/com/dasiolmapserver/dasiolmap/dasiolstore/repository/DasiolStoreRepository.java
@@ -3,7 +3,7 @@ package com.dasiolmapserver.dasiolmap.dasiolstore.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.dasiolmapserver.dasiolmap.dasiolstore.domain.entity.DasiolStoreEntity;
+import com.dasiolmapserver.dasiolmap.DasiolStore.domain.entity.DasiolStoreEntity;
 
 @Repository
 public interface DasiolStoreRepository extends JpaRepository<DasiolStoreEntity, String> {
