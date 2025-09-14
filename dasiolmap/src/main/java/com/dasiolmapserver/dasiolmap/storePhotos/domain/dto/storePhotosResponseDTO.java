4a4389af6c32517fe29d16e158fa12d531369238
InoPiaN <jh_95@naver.com>
2025-09-14 19:51:57 +0900

    bookmarks add

---
+++ dasiolmap/src/main/java/com/dasiolmapserver/dasiolmap/storePhotos/domain/dto/storePhotosResponseDTO.java
@@ -0,0 +1,8 @@
+package com.dasiolmapserver.dasiolmap.storePhotos.domain.dto;
+
+public class storePhotosResponseDTO {
+    private Integer storePhotoID;
+    private Integer storeId;
+    private Integer reviewId;
+
+}
