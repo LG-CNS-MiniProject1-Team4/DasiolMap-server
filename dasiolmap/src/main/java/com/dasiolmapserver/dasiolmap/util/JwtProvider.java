ce6a7cbe48ed4c1fb1ed193cd971044b61c962f0
Startingfindmistakesamsung <dudgh2328@gmail.com>
2025-09-15 11:24:32 +0900

    Refactor JwtProvider to improve debug logging and clean up code structure

--- dasiolmap/src/main/java/com/dasiolmapserver/dasiolmap/util/JwtProvider.java
+++ dasiolmap/src/main/java/com/dasiolmapserver/dasiolmap/util/JwtProvider.java
@@ -15,48 +15,25 @@ public class JwtProvider {
     @Value("${jwt.secret}")
     private String secret;
 
-    private Key getStringKey() {
-<<<<<<< HEAD
-        System.out.println("[debug] >>> JwtProvider secret : "+secret);
-=======
+     private Key getStringKey() {
         System.out.println("[debug] JwtProvider secret : " + secret);
->>>>>>> feature/#4-store
         return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
+
 
     public String generateAccessToken(String email) {
         System.out.println("[debug] >>> JwtProvider generateAccessToken");
         return Jwts.builder()
-<<<<<<< HEAD
-                    .setSubject(email)
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                    .signWith(getStringKey())
-                    .compact() ;
-=======
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(getStringKey())
                 .compact();
->>>>>>> feature/#4-store
     }
 
     public String generateRefreshToken(String email) {
         System.out.println("[debug] >>> JwtProvider generateRefreshToken");
         return Jwts.builder()
-<<<<<<< HEAD
-                    .setSubject(email)
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
-                    .signWith(getStringKey())
-                    .compact() ;
-    }
-
-
-}
-
-=======
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
@@ -64,5 +41,6 @@ public class JwtProvider {
                 .compact();
     }
 
+
 }
->>>>>>> feature/#4-store
+
