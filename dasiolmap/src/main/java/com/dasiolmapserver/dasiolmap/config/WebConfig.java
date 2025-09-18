0e0d9ff44157e7c155a2fe4c31f7ccbb528def0e
Startingfindmistakesamsung <dudgh2328@gmail.com>
2025-09-18 09:24:11 +0900

    update

--- dasiolmap/src/main/java/com/dasiolmapserver/dasiolmap/config/WebConfig.java
+++ dasiolmap/src/main/java/com/dasiolmapserver/dasiolmap/config/WebConfig.java
@@ -1,21 +1,19 @@
-// package com.dasiolmapserver.dasiolmap.config;
+package com.dasiolmapserver.dasiolmap.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
-// @Override
-// public void addCorsMappings(CorsRegistry registry) {
-// registry.addMapping("/**") // 모든 API 경로
-// .allowedHeaders("*")
-// .allowedOriginPatterns("http://localhost:3000")
-// .allowedMethods("*")
-// .allowCredentials(true) // 인증허용
-// .exposedHeaders("Authorization", "Refresh-Token");
-
-// }
-
-// }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 API 경로
+            .allowedHeaders("*")
+            .allowedOriginPatterns("http://localhost:3000") // 프론트엔드 주소
+            .allowedMethods("*")
+            .allowCredentials(true) // 인증정보 허용
+            .exposedHeaders("Authorization", "Refresh-Token");
+    }
+}
